7735031604693a6f8e629fd71bdb8ab916c742f1
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-11-20 20:15:16 +0100

    add driven ports

---
+++ cmd/users/application/ports/driven/followers_repository.go
@@ -0,0 +1,8 @@
+package ports
+
+import "context"
+
+type IFollowersRepository interface {
+	Follow(ctx context.Context, userID, followerID string) error
+	Unfollow(ctx context.Context, followerID, userID string) error
+}
