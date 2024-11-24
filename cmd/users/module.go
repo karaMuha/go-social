3e8e05afdb134b67c8478ba97cfc6e5b9a41159e
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-11-24 13:49:47 +0100

    add api versioning

--- cmd/users/module.go
+++ cmd/users/module.go
@@ -7,7 +7,7 @@ import (
 	"github.com/karaMuha/go-social/internal/monolith"
 	"github.com/karaMuha/go-social/users/application"
 	"github.com/karaMuha/go-social/users/postgres"
-	"github.com/karaMuha/go-social/users/rest"
+	"github.com/karaMuha/go-social/users/rest/v1"
 )
 
 type Module struct{}
@@ -20,12 +20,14 @@ func (m *Module) Startup(ctx context.Context, mono monolith.IMonolith) error {
 	app := application.New(usersRepository)
 
 	// setup driver adapters
-	usersHandler := rest.NewUsersHandler(app)
-	setupRoutes(mono.Mux(), usersHandler)
+	usersHandlerV1 := rest.NewUsersHandlerV1(app)
+	setupRoutes(mono.Mux(), usersHandlerV1)
 	return nil
 }
 
-func setupRoutes(router *http.ServeMux, usersHandler rest.UsersHandler) {
-	router.HandleFunc("POST /users", usersHandler.RegisterUserHandler)
+func setupRoutes(router *http.ServeMux, usersHandlerV1 rest.UsersHandlerV1) {
+	v1 := http.NewServeMux()
+	v1.HandleFunc("POST /users", usersHandlerV1.RegisterUserHandler)
 
+	router.Handle("/v1/", http.StripPrefix("/v1", v1))
 }
