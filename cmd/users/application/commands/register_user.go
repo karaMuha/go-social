4eff66865044136c7229caaa3f9aa24c9e6d59c4
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-11-24 13:07:19 +0100

    comment code

--- cmd/users/application/commands/register_user.go
+++ cmd/users/application/commands/register_user.go
@@ -24,6 +24,9 @@ func NewRegisterUserCommand(usersRepo ports.IUsersRepsitory) RegisterUserCommand
 	}
 }
 
+// Check within the domain logic if email and username already exist.
+// Currently these checks depend on the postgres implementation.
+// Take case insensivity for email (and username?) into account.
 func (c RegisterUserCommand) RegisterUser(ctx context.Context, cmd RegisterUserDto) error {
 	registration, err := domain.RegisterUser(cmd.Username, cmd.Email, cmd.Password)
 	if err != nil {
