e30f8207f6961b1e4df08c3d5e6bd95d34f5659d
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-12-09 16:13:27 +0100

    add test for update post logic

--- cmd/posts/application/domain/posting_test.go
+++ cmd/posts/application/domain/posting_test.go
@@ -9,6 +9,7 @@ import (
 func TestCreatePost(t *testing.T) {
 	InitValidator()
 	userID := uuid.New()
+
 	tests := []struct {
 		testName string
 		title    string
@@ -33,6 +34,40 @@ func TestCreatePost(t *testing.T) {
 		}
 		if err != nil && !test.wantErr {
 			t.Errorf("Create post test error: want no error but got error for test case: %s", test.testName)
+		}
+	}
+}
+
+func TestUpdatePost(t *testing.T) {
+	InitValidator()
+	userID := uuid.New()
+	wrongUserID := uuid.New()
+	post, err := CreatePost("This is a Title", userID.String(), "This is the content")
+	if err != nil {
+		t.Errorf("Cannot prepare post for test: %v", err)
+	}
+
+	tests := []struct {
+		testName string
+		userID   string
+		title    string
+		content  string
+		wantErr  bool
+	}{
+		{"TestNoParameters", "", "", "", true},
+		{"TestWrongUser", wrongUserID.String(), "SomeRandomTitle", "SomeRandomContent", true},
+		{"TestNoTitle", userID.String(), "", "SomeRandomContent", true},
+		{"TestNoContent", userID.String(), "SomeRandomTitle", "", true},
+		{"TestSuccessfulUpdate", userID.String(), "SomeRandomTitle", "SomeRandomContent", false},
+	}
+
+	for _, test := range tests {
+		err := post.Update(test.title, test.content, test.userID)
+		if err == nil && test.wantErr {
+			t.Errorf("Update post test error: want error but got none for test case: %s", test.testName)
+		}
+		if err != nil && !test.wantErr {
+			t.Errorf("Update post test error: want no error but got error for test case: %s", test.testName)
 		}
 	}
 }
