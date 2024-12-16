6111cf932c6d10a6158eb9c9da850a2fef83ca50
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-12-16 11:58:23 +0100

    remove wildcard matcher

--- cmd/internal/monolith/endpoints.go
+++ cmd/internal/monolith/endpoints.go
@@ -1,35 +1,25 @@
 package monolith
 
-import "github.com/vodkaslime/wildcard"
+var protectedEndpoints map[string]bool
 
-var ProtectedRoutes map[string]bool
-var matcher *wildcard.Matcher
+func setProtectedEndpoints() {
+	protectedEndpoints = make(map[string]bool)
+	protectedEndpoints["POST /v1/users/signup-for-registration"] = false
+	protectedEndpoints["POST  /v1/users/confirm-registration"] = false
+	protectedEndpoints["GET  /v1/users/view-user-details"] = false
+	protectedEndpoints["POST  /v1/users/login"] = false
 
-func setProtectedRoutes() {
-	matcher = wildcard.NewMatcher()
-	ProtectedRoutes = make(map[string]bool)
-	ProtectedRoutes["POST /v1/users/signup-for-registration"] = false
-	ProtectedRoutes["POST  /v1/users/confirm-registration"] = false
-	ProtectedRoutes["GET  /v1/users/view-user-details"] = false
-	ProtectedRoutes["POST  /v1/users/login"] = false
+	protectedEndpoints["POST /v1/followers/follow-user"] = true
+	protectedEndpoints["POST /v1/followers/unfollow-user"] = true
+	protectedEndpoints["GET /v1/followers/list-followers-of-user"] = true
 
-	ProtectedRoutes["POST /v1/followers/follow-user"] = true
-	ProtectedRoutes["POST /v1/followers/unfollow-user"] = true
-	ProtectedRoutes["GET /v1/followers/list-followers-of-user"] = true
-
-	ProtectedRoutes["POST /v1/contents/post-content"] = true
-	ProtectedRoutes["GET /v1/contents/view-content-details"] = true
-	ProtectedRoutes["POST /v1/contents/update-content"] = true
-	ProtectedRoutes["POST /v1/contents/remove-content"] = true
-	ProtectedRoutes["GET /v1/contents/view-users-content"] = false
+	protectedEndpoints["POST /v1/contents/post-content"] = true
+	protectedEndpoints["GET /v1/contents/view-content-details"] = true
+	protectedEndpoints["POST /v1/contents/update-content"] = true
+	protectedEndpoints["POST /v1/contents/remove-content"] = true
+	protectedEndpoints["GET /v1/contents/view-users-content"] = false
 }
 
-func IsProtectedRoute(endpoint string) bool {
-	for i, v := range ProtectedRoutes {
-		if result, _ := matcher.Match(i, endpoint); result {
-			return v
-		}
-	}
-
-	return false
+func IsProtectedEndpoint(endpoint string) bool {
+	return protectedEndpoints[endpoint]
 }
